bcd3e3a51a2738eeca9afa5f5fc0cbe0a6468dd0
David Melgarejo <davidarcosm@hotmail.com>
2026-03-07 08:41:13 -0600

    vercel/analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+//Vercel Speed Insights & Analytics
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -152,6 +156,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
